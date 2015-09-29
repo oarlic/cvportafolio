ce832e11dd20d62a38cf3e4b99ef33968645598b
oarlic <ordaz.11r@gmail.com>
2015-09-29 13:40:38 -0500

    66

--- cont.php
+++ cont.php
@@ -1,18 +1,20 @@
 <?php
-
-// INI CORREO AL QUE SE ENVIAN LOS MENSAJES
-$mail_destinatario = 'ordaz.11r@gmail.com';
-// FIN CORREO AL QUE SE ENVIAN LOS MENSAJES
-
-if (isset ($_POST['enviar'])) {
-$headers .= "From: ".$_POST['email']. "";
-if ( mail ($mail_destinatario, $_POST['asunto'], "Mensaje enviado desde formulario".$_POST['']."\n \n". "E-Mail: ".$_POST['email']."\n". "Nombre: ".$_POST['nombre']."\n". "Asunto: ".stripcslashes ($_POST['asunto'])."\n". "Mensaje: ".stripcslashes ($_POST['mensaje']), $headers )) echo'
-
-    Mensaje enviado!
-'; 
-
-else echo '
-    Error! Por favor, inténtelo de nuevo mas tarde.
-'; 
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+$para = 'ordaz.11r@gmail.com';
+$titulo = 'ASUNTO DEL MENSAJE';
+$header = 'From: ' . $email;
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
+  
+if ($_POST['submit']) {
+if (mail($para, $titulo, $msjCorreo, $header)) {
+echo "<script language='javascript'>
+alert('Mensaje enviado, muchas gracias.');
+window.location.href = 'http://www.oarlic.com';
+</script>";
+} else {
+echo 'Falló el envio';
+}
 }
 ?>
